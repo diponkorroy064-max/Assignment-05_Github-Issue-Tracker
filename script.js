76c42658eaa9a7146e8cab9933b516ade94498da
diponkorroy064-max <diponkorroy064@gmail.com>
2026-03-07 18:23:26 +0600

    functioning login page

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('login-btn').addEventListener('click',()=>{
+    // get the username---
+    const usernameId = document.getElementById('username');
+    const username = usernameId.value;
+    console.log(username);
+
+    // get pasward---
+    const passwardId = document.getElementById('password');
+    const passward = passwardId.value;
+    console.log(passward);
+
+
+
+    // match usename and passward
+    if (username == "admin" && passward == "admin123") {
+        alert('passward successful');
+        window.location.assign('home.html');
+    }
+    else {
+        alert('Invalid info.');
+        return;
+    }
+})
