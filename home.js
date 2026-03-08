fa9d00d220df13ca7b1650ac5e0988b40d13162a
diponkorroy064-max <diponkorroy064@gmail.com>
2026-03-08 14:03:45 +0600

    add modal section

--- home.js
+++ home.js
@@ -1,7 +1,11 @@
+// get the id  of modal---
+// const modal = document.getElementById('my_modal');
+
 // loading all card documents---
 const loadAllIssues = fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
     .then((issues) => displayAllIssues(issues.data));
+
 
 
 // display all cards---
@@ -9,17 +13,38 @@ const displayAllIssues = (allData) => {
     // console.log(allData);
     const cardContainer = document.getElementById('card_container');
 
+
+    // display labels array--
+    const createElement = (arr) => {
+        const htmlElements = arr.map((el) => `<span class="badge badge-outline badge-warning">${el}</span>`);
+        // console.log(htmlElements);
+        
+        return htmlElements.join("");
+    };
     
 
     allData.forEach(data => {
         // console.log(data);
+        // console.log(data.status);
 
         const card = document.createElement('div');
-        card.className = "bg-white rounded-md shadow p-4 space-y-5";
+        // display border top color---
+        if (data.status=="open") {
+            // console.log("dipjol");
+            card.className = "bg-white rounded-md shadow p-4 space-y-5 border-t-3 border-t-green-500";
+        }
+        else {
+            card.className = "bg-white rounded-md shadow p-4 space-y-5 border-t-3 border-t-blue-500";
+        }
+
+        // showing the modal ---
+        card.onclick = () => loadModal(data.id);
+        // console.log(data.id);
+
         card.innerHTML = `
             <div class="flex justify-between">
                 <img src="assets/Open-Status.png" alt="">
-                <span class="badge badge-soft badge-warning">${data.priority}</span>
+                <span class="badge badge-outline badge-secondary">${data.priority}</span>
             </div>
 
             <div>
@@ -27,8 +52,8 @@ const displayAllIssues = (allData) => {
                 <p class="text-[12px] text-[#64748B] line-clamp-2">${data.description}</p>
             </div>
 
-            <div id="all_labels" class="">
-                
+            <div id="all_labels" class="flex gap-5 flex-wrap">
+                ${createElement(data.labels)}
             </div>
 
             <div class="border-t-2 border-gray-200 pt-2.5 space-y-2">
@@ -37,7 +62,73 @@ const displayAllIssues = (allData) => {
             </div>
         `;
         cardContainer.appendChild(card);
+
     });
+
 }
+
+
+// load all modal issues----
+const loadModal = async (issueId) => {
+
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`);
+
+    const singleIssue = await res.json();
+    // console.log(singleIssue);
+    
+    displayModal(singleIssue.data);
+
+    my_modal.showModal(issueId);
+};
+
+
+// display all modal issues---
+const displayModal = (issue) => {
+    console.log(issue);
+
+    // display labels array---
+    const createElement = (arr) => {
+        const htmlElements = arr.map((el) => `<span class="badge badge-outline badge-warning">${el}</span>`);
+        // console.log(htmlElements);
+
+        return htmlElements.join("");
+    };
+
+    const modalContainer = document.getElementById('modal_container');
+    modalContainer.innerHTML = `
+        <h2 class="font-bold text-2xl">${issue.title}</h2>
+        <div>
+            <span class="badge badge-success">${issue.status}</span>
+            <span>${issue.author}</span>
+            <span>${issue.updatedAt}</span>
+        </div>
+            
+        <div class="flex gap-2">${createElement(issue.labels)}</div>
+            
+        <p class="text-[14px]">${issue.description}</p>
+
+        <div class="flex justify-start gap-12 bg-[#F8FAFC] rounded-md p-2.5 shadow">
+            <div>
+                <h3 class="font-semibold">Assignee:</h3>
+                <p>${issue.author}</p>
+            </div>
+                
+            <div>
+                <h3 class="font-semibold">Priority:</h3>
+                <p>${issue.priority}</p>
+            </div>
+        </div>
+    `;
+};
+
+
+
+
+
+
+
+
+
+
 
 
